d46ed6f4c61f061685141a62166d319f7ae67947
Nayana2898 <64266639+Nayana2898@users.noreply.github.com>
2021-05-31 04:50:56 -0700

    Update Empp.java

--- Empp.java
+++ Empp.java
@@ -8,7 +8,7 @@ abstract class Employee
 	Scanner sc =  new Scanner(System.in);
 	public Employee( )
        	{
-                	      System.out.println("\nenter name:");
+                	System.out.println("\nenter name:");
                         name=sc.next();  
                         System.out.println("enter  your age:");
                         age=sc.nextInt();
@@ -24,64 +24,64 @@ abstract class Employee
 final class Clerk extends Employee
 {
        public Clerk( )
-                {
+       {
 	salary=8000;
 	job="Clerk";
-                }
+       }
 
       public void raise( )
-                {
+       {
 	salary = salary+1000;
 	System.out.println("\nsalary raised........");
-                }
+       }
 }
 final class Programmer extends Employee
 {
        public Programmer( )
-                {
+       {
 	salary=20000;
 	job="Programmer";
-                }
+       }
 
       public void raise( )
-                {
+       {
 	 salary = salary+5000;
 	 System.out.println("\nsalary raised........");
-                }
+       }
 }
 final class Manager extends Employee
 {
        public Manager()
-                {
+      {
 	salary=25000;
 	job="Manager";
-                }
+      }
 
       public void raise( )
-                {
+      {
 	salary = salary+7000;
 	System.out.println("\nsalary raised........");
-                }
+      }
 }
 public class Empp
 {
 public static void main(String[] args)
 { 	
 	Scanner sc =  new Scanner(System.in);
-                  Employee[] e = new Employee[10];
+        Employee[] e = new Employee[10];
 	int c, c1=0, i=0;
 	do
 	{
 	     System.out.println("\n1.Create");
-                       System.out.println("2.Display");
-                       System.out.println("3.Raise salary");
+             System.out.println("2.Display");
+             System.out.println("3.Raise salary");
 	     System.out.println("4.Exit");
-                	     System.out.println("Enter your choice");
-                       c=sc.nextInt();
-                       switch(c)
+             System.out.println("Enter your choice");
+             c=sc.nextInt();
+             switch(c)
 	    {
-		case 1:     do
-		               {
+		case 1:do
+		       {
 			System.out.println("\n1.Clerk");
 			System.out.println("2.Programmer");
 			System.out.println("3.Manager");
@@ -104,23 +104,27 @@ public static void main(String[] args)
 		          break;
 				
   		case 2: try
-		           {
+		        {
 			for(Employee x: e)
 			{
 			      if(x!=null)
 			      x.disp();
 			}
-		            }catch (Exception ex) {
+		        }
+		        catch (Exception ex)
+			{
 			 System.out.println("No records...");
 			}
 		case 3: try
-		           {
+		        {
 			for(Employee x: e)
 			{
 			      if(x!=null)
 			      x.raise();
 			}
-		            }catch (Exception ex) {
+		        }
+			catch (Exception ex) 
+			{
 			 System.out.println("No records...");
 			}
 		case 4: System.out.println("\nExisting..........!!");
